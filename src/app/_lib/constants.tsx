ee4663095411eeb17d30589c3e68662901f96c61
MatanB27 <69850880+MatanB27@users.noreply.github.com>
2024-03-06 13:41:13 +0200

    working on header mobiel

--- src/app/_lib/constants.tsx
+++ src/app/_lib/constants.tsx
@@ -1,3 +1,5 @@
+export const ROOT_ROUTE:                        string = '/'
+
 export const LOGIN_ROUTE:                       string = '/login'
 export const REGISTER_ROUTE:                    string = '/register'
 export const FORGET_PASSWORD_ROUTE:             string = '/forget-password'
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,6 +3,10 @@
 import { FORGET_PASSWORD_ROUTE, LOGIN_ROUTE, REGISTER_ROUTE } from "@/app/_lib/constants"
 import { usePathname } from "next/navigation"
 import styles from './header.module.scss'
+import ChefHatIcon from '@/assets/icons/chef-hat.svg'
+import { ROOT_ROUTE } from "@/app/_lib/constants"
+import menuIcon from '@/assets/icons/menu.svg'
+import Link from "next/link"
 
 type HeaderProps = {
     props?: {}
@@ -14,13 +18,25 @@ export default function Header(props: HeaderProps) {
     const pathname = usePathname()
     const shouldHeaderExists = pathname && !routesWithoutHeader.includes(pathname)
     
+    const headerLogo = () => {
+        return (
+            <Link className={styles['logo-container']} href={ROOT_ROUTE}>
+                <img className={styles['icon']} src={ChefHatIcon.src} alt="chef-logo"/>
+                <span className={styles['logo-text']}>uChef</span>
+            </Link>
+        )
+    }
     if(!shouldHeaderExists) {
         return <></>
     }
 
     return (
     <header className={styles['header']}>
-        
+        {headerLogo()}
+
+        <button className={styles['menu']}>
+            <img className={styles['menu-icon']} src={menuIcon.src} alt="menu"/>
+        </button>
     </header>
     )
 }
