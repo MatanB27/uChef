6636412b1d303f8102e5b6c6eb7cb257840bb320
MatanB27 <69850880+MatanB27@users.noreply.github.com>
2024-03-12 14:44:12 +0200

    change footer color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { LOGIN_ROUTE } from "./_lib/constants";
 export default function Home() {
     //TODO: remove the style when finish
     return (
-        <main style={{height: '150vh'}}>
+        <main style={{minHeight: '100vh'}}>
             This is the Homepage
             <Link href={LOGIN_ROUTE}>Login here!</Link>
         </main>
