bdc0b3944554ec876218ba20008695efb21e204f
MatanB27 <69850880+MatanB27@users.noreply.github.com>
2024-03-30 14:52:24 +0300

    more users files

--- server/src/handlers/users.ts
+++ server/src/handlers/users.ts
@@ -1,4 +1,7 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express-serve-static-core"
+import { CreaterUserDto } from "../dtos/CreateUser.tdo"
+import { CreateUserQueryParams } from "../types/query-params"
+import { User } from "../types/user-response"
 
 // api/users
 export function getUsers(req: Request, res: Response) {
@@ -10,6 +13,9 @@ export function getUserById(req: Request, res: Response) {
     res.send({user: 'user1234'})
 }
 
-export function createUser(req: Request, res: Response) {
-    
+export function createUser(req: Request<{}, {}, CreaterUserDto, CreateUserQueryParams>, 
+    res: Response<User>) {
+    return res.status(201).send(
+        {id: 1, firstName: 'matan', lastName: 'baruch', email: 'mmmmm@gmail.com', phone:'0521111111'}
+        )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express, {NextFunction, Request, Response} from 'express'
+import express from 'express'
 import usersRouter from './routes/users'
 
 const PORT = 8000;
