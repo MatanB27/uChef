f5648d65bf7bb126a3103b14e0b7a55f7efcc07c
MatanB27 <69850880+MatanB27@users.noreply.github.com>
2024-04-03 22:54:57 +0300

    Added blurry effect and make animation smoother

--- client/src/popups/index.tsx
+++ client/src/popups/index.tsx
@@ -1,7 +1,8 @@
 import { useDispatch, useSelector } from 'react-redux'
 import styles from './index.module.scss'
 import Actions from '@/redux/actions'
-
+import CloseIcon from '@/assets/icons/close.svg'
+import clsx from 'clsx'
 interface PopupProps<T> {
     data: T, //TODO: add specific data (like text, subText, button text... etc...)
     type: string,
@@ -17,18 +18,24 @@ export function Popup<T>(props: PopupProps<T>) {
     const popup = useSelector((store: any) => store.popup)
     const dispatch = useDispatch()
     
-    const open = () => {
-        dispatch(Actions.popup(false))
-    }   
-    
-    const close = () => {
-        dispatch(Actions.popup(false))
+    const onClose = () => {
+        dispatch(Actions.setPopup(false))
     }
 
-    if(!popup) { return <></>} // TODO: need to change to make it
+    const popupStyle = {
+        transform: popup ? 'translateY(0)' : 'translateY(100%)',
+        transition: 'transform 0.3s ease-in-out'
+    };
+
     return (
-        <div className={styles['popup']}>
-            Check here for popup
-        </div>
+        <>
+            <button className={clsx(styles['backdrop'], popup ? styles['active'] : styles['not-active'])} onClick={onClose}/>
+            <div style={popupStyle} className={styles['popup']}>
+                <button className={styles['close-btn']} onClick={onClose}>
+                    <img src={CloseIcon.src}/>
+                </button>
+                Add type here
+            </div>
+        </>
     )
 }
