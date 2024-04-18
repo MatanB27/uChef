586d32601398c7ee06bb939b61c43b22482fd95b
MatanB27 <69850880+MatanB27@users.noreply.github.com>
2024-04-18 18:26:26 +0300

    added react query and devtools to the project

--- client/src/app/_app.tsx
+++ client/src/app/_app.tsx
@@ -2,19 +2,52 @@
 
 import store from "@/redux";
 import { Provider } from "react-redux";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState, lazy, useEffect, Suspense } from "react";
 
 type MyAppProps = {
     children: React.ReactNode
 }
+
+/**
+ * The React Query Devtool is enabled only in development mode.
+ * This code ensures it can also be enabled in production, but only after calling 'window.toggleDevtools()'.
+ */
+const ReactQueryDevtoolsProduction = lazy(() =>
+    import('@tanstack/react-query-devtools/build/modern/production.js').then(
+        (d) => ({
+        default: d.ReactQueryDevtools,
+        }),
+    ),
+)
 
 export default function MyApp(props: MyAppProps) {
 
     const {
         children
     } = props
+
+    const [showDevtools, setShowDevtools] = useState<boolean>(false)
+    const queryClient = new QueryClient()
+
+    useEffect(() => {
+        // @ts-expect-error
+        window.toggleDevtools = () => setShowDevtools((old) => !old)
+    }, [])
+    
+    
     return (
         <Provider store={store}>
-            {children}
+            <QueryClientProvider client={queryClient}>
+                {children}
+                <ReactQueryDevtools initialIsOpen={false} buttonPosition={'bottom-left'}/>
+                    {showDevtools && (
+                        <Suspense fallback={null}>
+                        <ReactQueryDevtoolsProduction />
+                        </Suspense>
+                    )}
+            </QueryClientProvider>
         </Provider>
     )
 }
